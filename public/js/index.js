76dd1a93e6b79bdad2a73dab802ccc918f136c9e
Aminat.A <aminatadesola@gmail.com>
2020-06-04 10:29:44 -0500

    Setting up api routes for index

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,53 @@
-// API
+const userInfo = $("form.userInput");
+const addressInput = $(".address");
+const zipcodeInput = $(".zipCode");
 
-const apiKey = "VyXiMxXtP3oty4G8rjGqLCFpJq5jVDzI";
-const mapQuest = "http://www.mapquestapi.com/geocoding/v1/address?key=" + apiKey + "Y&street=1600+Pennsylvania+Ave+NW&city=Washington&state=DC&postalCode=20500";
-
-
-console.log(mapQuest);
-
-
+let userData;
+// INDEX JS
 
 $(document).ready(() => {
   // Getting references to our form and input
-  const userInfo = $("form.userInput");
-  const addressInput = $(".address");
-  const zipcodeInput = $(".zipCode");
 
   // When the userInput button is clicked, we validate the address and zipcode are not blank
   userInfo.on("submit", event => {
     event.preventDefault();
-    const userData = {
-      userInfo: addressInput.val().trim(),
+     userData = {
+      address: addressInput.val().trim(),
       zipcode: zipcodeInput.val().trim()
     };
-    
-    if (!userData.address || !userData.zipcode) {
-      console.log("Success!")
-      return;
-    }
-    // If we have an address and zipcode, run the userInputUser function
-    userInputUser(userData.address, userData.zipcode);
+  
+    if (userData.address || userData.zipcode) {
+     
+  userInputUser(userData.address, userData.zipcode);
     addressInput.val("");
     zipcodeInput.val("");
+   
+    }
+    // If we have an address and zipcode, run the userInputUser function
+   
     
   });
   
   // Does a post to the userInput route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function userInputUser(address, zipcode) {
+    
+ 
     $.post("/api/userInput", {
       address: address,
       zipcode: zipcode
     })
-      .then(() => {
-        window.location.replace("/restaurant_list");
-        // If there's an error, handle it by throwing up a bootstrap alert
-      })
-      .catch(handleLoginErr);
+    .then(() => {
+      window.location.replace("/restaurant_list");
+      // If there's an error, handle it by throwing up a bootstrap alert
+    })
+    .catch(handleLoginErr);
   }
-
+  
   function handleLoginErr(err) {
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
-
+  
 });
+
